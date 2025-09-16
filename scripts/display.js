d90541fe911dad200437df69479b5f03eff8cce2
jaredstaiert <jwstaiert@gmail.com>
2025-09-16 15:06:45 -0500

    working control ui

--- scripts/display.js
+++ scripts/display.js
@@ -1,19 +1,30 @@
 Hooks.on("getSceneControlButtons", (controls) => {
-  controls.push({
-    name: "delete-resource",
-    title: "Delete Resource",
-    icon: "fas fa-trash",
-    layer: "controls",
-    tools: [{
-      name: "delete-resource-tool",
-      title: "Delete Resource",
-      icon: "fas fa-trash-alt",
-      onClick: () => {
-        // Your delete logic here
-        ui.notifications.info("Delete button clicked!");
-        // Example: delete a tile or file reference
-      },
-      button: true
-    }]
-  });
+  controls.imagedisplay = {
+    activeTool: "popup",
+    name: "imagedisplay",
+    title: "Display Images",
+    icon: "fa-solid fa-images",
+    order: 11,
+    visible: game.user.isGM,
+    onChange: (event, active) => {
+        console.log("group on change");
+    },
+    tools: {
+      popup: {
+        name: "popup",
+        order: 1,
+        title: "Popup Image",
+        icon: "fa-solid fa-image",
+        button: true,
+        onChange: (event, active) => {
+            if (active) {
+                console.log("clicked");
+            }
+        }
+      }
+    }
+  };
+
+  console.log(controls);
 });
+
