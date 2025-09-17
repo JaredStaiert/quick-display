b4031c48cd17b8b4ac54f545a949ab381a45bdd7
jaredstaiert <jwstaiert@gmail.com>
2025-09-17 12:15:04 -0500

    Popup working

--- scripts/display.js
+++ scripts/display.js
@@ -1,5 +1,5 @@
 /**
- * Creates new control group and control by hooking into getScenControlButtons
+ * Creates new control group and tools by hooking into getScenControlButtons
  */
 Hooks.on("getSceneControlButtons", (controls) => {
     controls.imagedisplay = {
@@ -19,7 +19,7 @@ Hooks.on("getSceneControlButtons", (controls) => {
                 button: true,
                 onChange: (event, active) => {
                     if (active) {
-                        getUrlFromUser();
+                        getUrlDialog();
                     }
                 }
             }
@@ -29,16 +29,7 @@ Hooks.on("getSceneControlButtons", (controls) => {
     console.log(controls);
 });
 
-async function uploadAndDisplayImage() {
-    const url = await getUrlFromUser();
-
-    const uploadUrl = await importImage(decodeImageUrl(url));
-
-    await renderPopup(uploadUrl);
-
-}
-
-async function getUrlFromUser() {
+async function getUrlDialog() {
     new foundry.applications.api.DialogV2({
         window: { title: "Image Popup" },
         content: `
@@ -48,26 +39,65 @@ async function getUrlFromUser() {
             action: "choice",
             label: "Share With Players!",
             default: false,
-            callback: (event, button, dialog) => button.form.elements.choice.value
+            callback: (event, button, dialog) => {
+                button.form.elements.choice.value
+            }
         }],
         submit: result => {
 
+            // Must match DialogV2 content id.
             const fileInput = document.getElementById("share");
             const file = fileInput.files[0];
+            const delimiterIndex = file.name.lastIndexOf(".");
 
-            const imageFile = new File([blob], `temp`)
+            let newFileName;
 
-            if (file) {
-                FilePicker.upload("data", `worlds/${game.world.id}`, imageFile, { notify: false })
-                    .then(response => {
-                        console.log("Upload successful:", response.path);
-                        // The response.path contains the full path to the uploaded file
-                    })
-                    .catch(error => {
-                        console.error("Upload failed:", error);
-                    });
+            if ((delimiterIndex < 1) || (delimiterIndex === file.name.length - 1)) {
+                // Ignore files like: ".local", "missingExtension."
+                newFileName = file.name;
             }
 
+            // Get proper slice index
+            newFileName = `temp${file.name.slice(-(file.name.length - delimiterIndex))}`;
+
+            if (file) {
+                try {
+                    foundry.applications.apps.FilePicker.implementation.upload(
+                        "data", 
+                        `worlds/${game.world.id}`,
+                        new File([file], newFileName, { type: file.type }), 
+                        { notify: false });
+                } catch {
+                    ui.notifications.warn("Image upload unsuccessful");
+                }
+                // Render image
+                renderPopup(`worlds/${game.world.id}/${newFileName}`);
+
+            }
         }
     }).render({ force: true });
+}
+
+/**
+ * Renders ImagePopout object using the provided image url.
+ * @param {string} imageUrl Formatted image url.
+ */
+async function renderPopup(imageUrl) {
+  const ip = new foundry.applications.apps.ImagePopout({
+    src: imageUrl,
+    window: {title: "Shared Image"}
+  });
+  await ip.render(true);
+  await ip.shareImage();
+}
+
+/**
+ * Decodes url and removes unecessary parentheses
+ * @param {string} rawUrl Unformatted image url.
+ * @returns Image url without parentheses encoding or parentheses.
+ */
+function decodeImageUrl(rawUrl) {
+    const decodedUrl = decodeURIComponent(rawUrl || "");
+    const cleanedUrl = decodedUrl.replace(/^"|"$/g, "");
+    return cleanedUrl;
 }
