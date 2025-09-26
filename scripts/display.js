207375ddccb47cd026f015e4b41b7615bc42d9bb
jaredstaiert <jwstaiert@gmail.com>
2025-09-26 16:00:40 -0500

    Added cachebust parameter to image path in ImagePopout

--- scripts/display.js
+++ scripts/display.js
@@ -2,120 +2,124 @@
  * Creates new control group and tools by hooking into getScenControlButtons
  */
 Hooks.on("getSceneControlButtons", (controls) => {
-    console.log(controls);
+  console.log(controls);
 
-    controls.imagedisplay = {
-        active: false,
-        activeTool: "div",
-        name: "imagedisplay",
-        title: "Display Images",
-        icon: "fa-solid fa-images",
-        order: 20,
-        visible: game.user.isGM,
-        onChange: (event, active) => { },
-        onToolChange: () => { },
-        tools: {
-            div: {
-                name: "div",
-                order: 0,
-                title: "",
-                icon: "fa-solid fa-toolbox",
-                button: true,
-                onChange: (event, active) => {
-                    /** Currently this tool does nothing. 
-                     * Foundry requires a tool to be active, but since
-                     * the tools below are buttons they execute when the 
-                     * toolbox is opened which is not desireable behavior. 
-                     * Will investigate toggle logic
-                     * to convert buttons to toggle UI elements.
-                    */
-                }
-            },
-            popup: {
-                name: "popup", 
-                order: 1,
-                title: "popup",
-                icon: "fa-solid fa-image",
-                button: true,
-                onChange: (event, active) => {
-                    if (active) {
-                        console.log(controls);
-                        uploadAndDisplayDialog("Popup");
-                    }
-                }
-            },
-            chat: {
-                name: "chat",
-                order: 2,
-                title: "chat",
-                icon: "fa-solid fa-comments",
-                button: true,
-                onChange: (event, active) => {
-                    if (active) {
-                        uploadAndDisplayDialog("Chat");
-                    }
-                }
-            }
-        }
-    };
+  controls.imagedisplay = {
+    active: false,
+    activeTool: "div",
+    name: "imagedisplay",
+    title: "Display Images",
+    icon: "fa-solid fa-images",
+    order: 20,
+    visible: game.user.isGM,
+    onChange: (event, active) => {},
+    onToolChange: () => {},
+    tools: {
+      div: {
+        name: "div",
+        order: 0,
+        title: "",
+        icon: "fa-solid fa-toolbox",
+        button: true,
+        onChange: (event, active) => {
+          /** Currently this tool does nothing.
+           * Foundry requires a tool to be active, but since
+           * the tools below are buttons they execute when the
+           * toolbox is opened which is not desireable behavior.
+           * Will investigate toggle logic
+           * to convert buttons to toggle UI elements.
+           */
+        },
+      },
+      popup: {
+        name: "popup",
+        order: 1,
+        title: "popup",
+        icon: "fa-solid fa-image",
+        button: true,
+        onChange: (event, active) => {
+          if (active) {
+            console.log(controls);
+            uploadAndDisplayDialog("Popup");
+          }
+        },
+      },
+      chat: {
+        name: "chat",
+        order: 2,
+        title: "chat",
+        icon: "fa-solid fa-comments",
+        button: true,
+        onChange: (event, active) => {
+          if (active) {
+            uploadAndDisplayDialog("Chat");
+          }
+        },
+      },
+    },
+  };
 });
 
 /**
  * Displays dialog prompting user to choose image on their PC. Renders according to given option.
- * @param {"Popup" | "Chat"} renderOption 
+ * @param {"Popup" | "Chat"} renderOption
  */
 async function uploadAndDisplayDialog(renderOption) {
-    new foundry.applications.api.DialogV2({
-        window: { title: `Image ${renderOption}` },
-        content: `
+  new foundry.applications.api.DialogV2({
+    window: { title: `Image ${renderOption}` },
+    content: `
             <label><input type="file" id="share" name="choice" value="" checked></label>
         `,
-        buttons: [{
-            action: "choice",
-            label: "Share With Players!",
-            default: false,
-            callback: (event, button, dialog) => {
-                button.form.elements.choice.value
-            }
-        }],
-        submit: result => {
+    buttons: [
+      {
+        action: "choice",
+        label: "Share With Players!",
+        default: false,
+        callback: (event, button, dialog) => {
+          button.form.elements.choice.value;
+        },
+      },
+    ],
+    submit: (result) => {
+      // Must match DialogV2 content id.
+      const fileInput = document.getElementById("share");
+      const file = fileInput.files[0];
+      const delimiterIndex = file.name.lastIndexOf(".");
 
-            // Must match DialogV2 content id.
-            const fileInput = document.getElementById("share");
-            const file = fileInput.files[0];
-            const delimiterIndex = file.name.lastIndexOf(".");
+      let newFileName;
 
-            let newFileName;
+      if (delimiterIndex < 1 || delimiterIndex === file.name.length - 1) {
+        // Ignore files like: ".local", "missingExtension."
+        newFileName = file.name;
+      }
 
-            if ((delimiterIndex < 1) || (delimiterIndex === file.name.length - 1)) {
-                // Ignore files like: ".local", "missingExtension."
-                newFileName = file.name;
-            }
+      // Get proper slice index
+      newFileName = `temp${file.name.slice(
+        -(file.name.length - delimiterIndex)
+      )}`;
 
-            // Get proper slice index
-            newFileName = `temp${file.name.slice(-(file.name.length - delimiterIndex))}`;
-
-            if (file) {
-                try {
-                    foundry.applications.apps.FilePicker.implementation.upload(
-                        "data", 
-                        `worlds/${game.world.id}`,
-                        new File([file], newFileName, { type: file.type }), 
-                        { notify: false });
-                } catch {
-                    ui.notifications.warn("Image upload unsuccessful");
-                }
-                // Render image
-
-                if (renderOption === "Popup") {
-                    renderPopup(`worlds/${game.world.id}/${newFileName}`);
-                }
-                if (renderOption === "Chat") {
-                    renderChat(`worlds/${game.world.id}/${newFileName}`);
-                }
-            }
+      if (file) {
+        try {
+          foundry.applications.apps.FilePicker.implementation.upload(
+            "data",
+            `worlds/${game.world.id}`,
+            new File([file], newFileName, { type: file.type }),
+            { notify: false }
+          );
+        } catch {
+          ui.notifications.warn("Image upload unsuccessful");
         }
-    }).render({ force: true });
+        // Render image
+
+        if (renderOption === "Popup") {
+          renderPopup(`worlds/${game.world.id}/${newFileName}`);
+        }
+        if (renderOption === "Chat") {
+          renderChat(`worlds/${game.world.id}/${newFileName}`);
+        }
+      }
+    },
+  }).render({ force: true });
 }
 
 /**
@@ -123,9 +127,11 @@ async function uploadAndDisplayDialog(renderOption) {
  * @param {string} imageUrl Formatted image url.
  */
 async function renderPopup(imageUrl) {
+  const cacheBustUrl = `${imageUrl}?_cb=${Date.now()}`;
+
   const ip = new foundry.applications.apps.ImagePopout({
-    src: imageUrl,
-    window: {title: "Shared Image"}
+    src: cacheBustUrl,
+    window: { title: "Shared Image" },
   });
   await ip.render(true);
   await ip.shareImage();
@@ -140,7 +146,7 @@ async function renderChat(imageUrl) {
     user: game.user.id,
     speaker: ChatMessage.getSpeaker(),
     content: `<img src="${imageUrl}" style="max-width: 100%" />`,
-    type: CONST.CHAT_MESSAGE_STYLES.OOC
+    type: CONST.CHAT_MESSAGE_STYLES.OOC,
   });
 }
 
@@ -150,7 +156,7 @@ async function renderChat(imageUrl) {
  * @returns Image url without parentheses encoding or parentheses.
  */
 function decodeImageUrl(rawUrl) {
-    const decodedUrl = decodeURIComponent(rawUrl || "");
-    const cleanedUrl = decodedUrl.replace(/^"|"$/g, "");
-    return cleanedUrl;
+  const decodedUrl = decodeURIComponent(rawUrl || "");
+  const cleanedUrl = decodedUrl.replace(/^"|"$/g, "");
+  return cleanedUrl;
 }
